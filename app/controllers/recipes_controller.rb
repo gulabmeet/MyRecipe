b2893c6c60802c440a508e8a185d2b7c3c52284c
Gulab S <md_glab@hotmail.com>
2015-08-21 10:13:05 +0000

    Added untracked files

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,50 @@
+class RecipesController < ApplicationController
+
+    def index
+    @recipes = Recipe.all
+    
+    end
+    
+    def show
+        
+        @recipe = Recipe.find(params[:id])
+    end
+    
+    def new
+        @recipe = Recipe.new
+    end
+
+    def create
+        
+        @recipe = Recipe.new(recipe_params)
+        @recipe.chef = Chef.find(2)          # we can not create only recipe without chef id
+        
+        if @recipe.save
+            flash[:success] = "Your Recipe was created successfully!"
+            redirect_to recipes_path
+        else
+            render :new
+        end
+        
+    end
+    
+    def edit
+        @recipe = Recipe.find(params[:id])
+    end
+    
+    def update
+        @recipe = Recipe.find(params[:id])
+        if @recipe.update(recipe_params)
+            flash[:success] = "Recipe Updated successfully"
+            redirect_to recipe_path(@recipe)
+        else
+            render :edit
+        end
+    end
+    
+    private
+    
+     def recipe_params
+         params.require(:recipe).permit(:name, :summary, :description)
+     end
+end
